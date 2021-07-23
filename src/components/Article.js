ad1894d3bfb0d61226dbce6fa900fddfbb20b288
Maida <maida.maxime@gmail.com>
2021-07-23 13:18:07 +0200

    Formatage de l'affichage de la date

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,12 +1,20 @@
 import React from "react";
 
 const Article = ({ article }) => {
-  console.log(article);
+  const dateParser = (date) => {
+    let newDate = new Date(date).toLocaleDateString("fr-FR", {
+      day: "numeric",
+      month: "long",
+      year: "numeric",
+    });
+    return newDate;
+  };
+
   return (
     <div className="article">
       <div className="card-header">
         <h3>{article.author}</h3>
-        <em>Posté le {article.date}</em>
+        <em>Posté le {dateParser(article.date)}</em>
       </div>
       <p>{article.content}</p>
       <div className="btn-container">
